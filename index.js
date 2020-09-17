8bfb97dd9063ed29751776e7e5536311956c0106
khrisparrales <lecristianot@gmail.com>
2020-09-17 16:35:24 -0500

    connect to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3009;
+const mongoose = require("mongoose");
+
+//connect to mongodb
+mongoose
+    .connect(
+        "mongodb+srv://admin:superadmin@cluster0.d2xlq.mongodb.net/Lighthouse?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }
+
+    )
+    .then(() => {
+        console.log("Connect to mongodb success");
+    })
+    .catch(() => {
+        console.log("Connect to mongodb faill");
+    });
+
+//listen server express
 app.listen(PORT, () => {
     console.log("Server started on PORT: ", PORT);
-})
+});
